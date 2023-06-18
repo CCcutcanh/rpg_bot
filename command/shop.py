11ad2be84364ea77a61c6e433abcd8b7c2843282
CCcutcanh <ndinhducanh@gmail.com>
2023-06-18 21:50:22 +0700

    update

--- command/shop.py
+++ command/shop.py
@@ -18,7 +18,7 @@ class Shop(commands.Cog):
         try:
             data = await get_bank_data()
             coin = data[str(ctx.author.id)]["point"]
-            send = await ctx.send(f"**Đây là các vật phẩm hiện có trong Shop:**\n\n*==Vật phẩm hồi HP==*\n<:binhHP:1118551444497375292> | id: 01 | 25<:vang:1116221866273681510>\n\n*==Vật phẩm tăng số quái có thể săn==*\n <:kiemC1:1118523931406631023> | id: 02 | 80<:vang:1116221866273681510>\n<:kiemC2:1118524150756163686> | id: 03 | 110<:vang:1116221866273681510>\n<:kiemC3:1118524395766415370> | id: 04 | 150<:vang:1116221866273681510>\n==*Vật phẩm hỗ trợ pvp*==\n <:pvpkiem3:1119872683367202846> | id: 05 | 140<:vang:1116221866273681510>\n<:pvpkiem2:1119872536616898560> | id: 06 | 150<:vang:1116221866273681510>\n<:pvpkiem1:1119872240478068826> | id: 07 | 110<:vang:1116221866273681510>\n\nReply tin nhắn này với id vật phẩm bạn muốn mua để mua vật phẩm\n**Sử dụng lệnh wp <weapon_id> để xem thông tin chi tiết về vật phẩm\n*Vật phẩm bạn mua sẽ thay thế vật phẩm hiện tại bạn đang sử dụng ngoại trừ vật phẩm hồi HP*\nTổng số vàng hiện có: {coin}<:vang:1116221866273681510>")
+            send = await ctx.send(f"**Đây là các vật phẩm hiện có trong Shop:**\n\n*==Vật phẩm hồi HP==*\n<:binhHP:1118551444497375292> | id: 01 | 25<:vang:1116221866273681510>\n\n*==Vật phẩm tăng số quái có thể săn==*\n <:kiemC1:1118523931406631023> | id: 02 | 80<:vang:1116221866273681510>\n<:kiemC2:1118524150756163686> | id: 03 | 110<:vang:1116221866273681510>\n<:kiemC3:1118524395766415370> | id: 04 | 150<:vang:1116221866273681510>\n==*Vật phẩm hỗ trợ pvp*==\n <:pvpkiem3:1119872683367202846> | id: 05 | 140<:vang:1116221866273681510>\n<:pvpkiem2:1119872536616898560> | id: 06 | 150<:vang:1116221866273681510>\n<:pvpkiem1:1119872240478068826> | id: 07 | 110<:vang:1116221866273681510>\n\nReply tin nhắn này với id vật phẩm bạn muốn mua để mua vật phẩm\n**Sử dụng lệnh wp <weapon_id> để xem thông tin chi tiết về vật phẩm**\n*Vật phẩm bạn mua sẽ thay thế vật phẩm hiện tại bạn đang sử dụng ngoại trừ vật phẩm hồi HP*\nTổng số vàng hiện có: {coin}<:vang:1116221866273681510>")
             def check(m):
                 return m.author.id == ctx.author.id and m.channel == ctx.channel and m.reference is not None and m.reference.message_id == send.id
             choose = await self.bot.wait_for("message", check=check)
@@ -31,7 +31,7 @@ class Shop(commands.Cog):
             wp_choose = weapon[choose.content]
             if choose.content == "01":
                 if coin < wp_choose["price"]:
-                    return await ctx.send("**Bạn không đủ :vang: để mua vật phẩm này**")
+                    return await ctx.send("**Bạn không đủ <:vang:1116221866273681510> để mua vật phẩm này**")
                 if data[str(ctx.author.id)]["hp"] > 60:
                     return await ctx.send("**HP quả bạn đã quá 60 không thể sử dụng bình HP**")
                 data[str(ctx.author.id)]["hp"] += 40
